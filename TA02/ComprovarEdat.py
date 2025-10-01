f3ec299f445b6affee44c4365daaa4714f63c5c9
ITB2526-JosepPerez <josep.perez.7e7@itb.cat>
2025-10-01 09:38:50 +0200

    commit dels comentaris de cada programa

--- TA02/ComprovarEdat.py
+++ TA02/ComprovarEdat.py
@@ -1,6 +1,19 @@
-edat = int(input("Introdueix la teva edat: "))
+"""
+Programa: ComprovarEdat
+Autor: Josep
+01/10/2025
+Descripció:
+Aquest programa demana a l’usuari que introdueixi la seva edat
+i comprova si és major o menor d’edat. Finalment, mostra un missatge
+indicant que el programa ha finalitzat.
+"""
 
-if edat >= 18:
+edat = int(input("Introdueix la teva edat: ")) # Entrada de l'edat
+
+if edat >= 18:  # Condició per comprovar la majoria d'edat
     print("Ets major d'edat.")
 else:
     print("Encara ets menor d'edat.")
+
+
+print("Progrma Finalitzat") # Missatge final
--- TA02/helloWold.py
+++ TA02/helloWold.py
@@ -1,1 +1,9 @@
+"""
+Programa: helloWold
+Autor: Josep
+01/10/2025
+Descripció:
+Mostra un missatge per pantalla Hello World.
+"""
+
 print("Hello World")
